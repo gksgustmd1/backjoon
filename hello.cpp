2bd4fb369efb10b38dba21868ffd672566712794
jindodog <gksgustmd1@naver.com>
2022-03-02 18:00:03 +0900

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <deque>
+#include <queue>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << "hello";
+
+}
+
